14bae3bbcff18dc73ddbb55d210c518617f8f152
KD-Web-Design <contact.kd.webdesign@gmail.com>
2024-10-24 13:24:09 +0200

    adding video

--- components/Video.tsx
+++ components/Video.tsx
@@ -4,7 +4,7 @@ import { list } from "@vercel/blob";
 export default function Video() {
   return (
     <Suspense fallback={<p>Loading video...</p>}>
-      <VideoComponent fileName="drone-mountain.mp4" />
+      <VideoComponent fileName="drone-mountain-c1Se7uME7LAFrIfdxdTMIGGN9GfBMa.mp4" />
     </Suspense>
   );
 }
@@ -14,10 +14,18 @@ async function VideoComponent({ fileName }: { fileName: string }) {
     prefix: fileName,
     limit: 1,
   });
+
   const { url } = blobs[0];
 
   return (
-    <video controls preload="none" aria-label="Video player">
+    <video
+      preload="auto"
+      aria-label="Video player"
+      loop
+      muted
+      autoPlay
+      controls
+    >
       <source src={url} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
